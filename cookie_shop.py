d8cd487b86d0e58554e24f60a0d8cea36e513319
justindenng <sethrollins8204@gmail.com>
2026-06-25 15:37:09 +0900

    dictionaries exercise.

--- cookie_shop.py
+++ cookie_shop.py
@@ -1,8 +1,10 @@
 """
 Functions necessary for running a virtual cookie shop.
 See README.md for instructions.
-Do not run this file directly.  Rather, run main.py instead.
+Do not run this file directly. Rather, run main.py instead.
 """
+
+import csv
 
 
 def bake_cookies(filepath):
@@ -15,7 +17,22 @@ def bake_cookies(filepath):
     :param filepath: The path to the data file.
     :returns: A list of all cookie data, where each cookie is represented as a dictionary.
     """
-    # write your code for this function below here.
+    cookies = []
+
+    with open(filepath, "r") as cookie_file:
+        cookie_reader = csv.DictReader(cookie_file)
+
+        for row in cookie_reader:
+            cookie = {
+                "id": int(row["id"]),
+                "title": row["title"].strip(),
+                "description": row["description"].strip(),
+                "price": float(row["price"].replace("$", "").strip())
+            }
+
+            cookies.append(cookie)
+
+    return cookies
 
 
 def welcome():
@@ -26,28 +43,35 @@ def welcome():
       We feed each according to their need.
 
     """
-    # write your code for this function below this line
+    print("Welcome to the Python Cookie Shop!")
+    print("We feed each according to their need.")
 
 
 def display_cookies(cookies):
     """
     Prints a list of all cookies in the shop to the user.
-    - Sample output - we show only two cookies here, but imagine the output continues for all cookiese:
+    - Sample output - we show only two cookies here, but imagine the output continues for all cookies:
         Here are the cookies we have in the shop for you:
 
           #1 - Basboosa Semolina Cake
-          This is a This is a traditional Middle Eastern dessert made with semolina and yogurt then soaked in a rose water syrup.
+          This is a traditional Middle Eastern dessert made with semolina and yogurt then soaked in a rose water syrup.
           Price: $3.99
 
           #2 - Vanilla Chai Cookie
-          Crisp with a smooth inside. Rich vanilla pairs perfectly with its Chai partner a combination of cinnamon ands ginger and cloves. Can you think of a better way to have your coffee AND your Vanilla Chai in the morning?
+          Crisp with a smooth inside. Rich vanilla pairs perfectly with its Chai partner.
           Price: $5.50
 
     - If doing the extra credit version, ask the user for their dietary restrictions first, and only print those cookies that are suitable for the customer.
 
     :param cookies: a list of all cookies in the shop, where each cookie is represented as a dictionary.
     """
-    # write your code for this function below this line
+    print("\nHere are the cookies we have in the shop for you:\n")
+
+    for cookie in cookies:
+        print(f"#{cookie['id']} - {cookie['title']}")
+        print(cookie["description"])
+        print(f"Price: ${cookie['price']:.2f}")
+        print()
 
 
 def get_cookie_from_dict(id, cookies):
@@ -58,7 +82,11 @@ def get_cookie_from_dict(id, cookies):
     :param cookies: a list of all cookies in the shop, where each cookie is represented as a dictionary.
     :returns: the matching cookie, as a dictionary
     """
-    # write your code for this function below this line
+    for cookie in cookies:
+        if cookie["id"] == id:
+            return cookie
+
+    return None
 
 
 def solicit_quantity(id, cookies):
@@ -76,7 +104,30 @@ def solicit_quantity(id, cookies):
     :param cookies: a list of all cookies in the shop, where each cookie is represented as a dictionary.
     :returns: The quantity the user entered, as an integer.
     """
-    # write your code for this function below this line
+    cookie = get_cookie_from_dict(id, cookies)
+
+    while True:
+        response = input(
+            f"My favorite! How many {cookie['title']}s would you like? "
+        )
+
+        try:
+            quantity = int(response)
+
+            if quantity > 0:
+                subtotal = quantity * cookie["price"]
+
+                print(
+                    f"Your subtotal for {quantity} "
+                    f"{cookie['title']} is ${subtotal:.2f}."
+                )
+
+                return quantity
+
+            print("Please enter a quantity greater than 0.")
+
+        except ValueError:
+            print("Please enter the quantity as a whole number.")
 
 
 def solicit_order(cookies):
@@ -95,13 +146,60 @@ def solicit_order(cookies):
 
     :returns: A list of the ids and quantities of each cookies the user wants to order.
     """
-    # write your code for this function below this line
+    order = []
+    first_order = True
+    finish_words = ["finished", "done", "quit", "exit"]
+
+    while True:
+        if first_order:
+            response = input(
+                "Please enter the number of any cookie "
+                "you would like to purchase: "
+            )
+        else:
+            response = input(
+                "Please enter the number of any other cookie you would "
+                'like to purchase (type "finished" if finished with '
+                "your order): "
+            )
+
+        response = response.strip().lower()
+
+        if response in finish_words:
+            break
+
+        try:
+            cookie_id = int(response)
+        except ValueError:
+            print(
+                "Please enter a valid cookie id number, "
+                'or type "finished" when you are done.'
+            )
+            continue
+
+        cookie = get_cookie_from_dict(cookie_id, cookies)
+
+        if cookie is None:
+            print("Please enter the id number of a cookie in the shop.")
+            continue
+
+        quantity = solicit_quantity(cookie_id, cookies)
+
+        sub_order = {
+            "id": cookie_id,
+            "quantity": quantity
+        }
+
+        order.append(sub_order)
+        first_order = False
+
+    return order
 
 
 def display_order_total(order, cookies):
     """
     Prints a summary of the user's complete order.
-    - Includes a breakdown of the title and quantity of each cookie the user ordereed.
+    - Includes a breakdown of the title and quantity of each cookie the user ordered.
     - Includes the total cost of the complete order, formatted to two decimal places.
     - Follows the format:
 
@@ -117,7 +215,23 @@ def display_order_total(order, cookies):
         -The Python Cookie Shop Robot.
 
     """
-    # write your code for this function below this line
+    print("\nThank you for your order. You have ordered:\n")
+
+    total = 0
+
+    for sub_order in order:
+        cookie = get_cookie_from_dict(sub_order["id"], cookies)
+        quantity = sub_order["quantity"]
+
+        print(f"-{quantity} {cookie['title']}")
+
+        subtotal = quantity * cookie["price"]
+        total += subtotal
+
+    print(f"\nYour total is ${total:.2f}.")
+    print("Please pay with Bitcoin before picking-up.")
+    print("\nThank you!")
+    print("-The Python Cookie Shop Robot.")
 
 
 def run_shop(cookies):
